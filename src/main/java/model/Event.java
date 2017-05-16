92bcbf8dc07994753788623c9b83bcb6c37b8ae1
Marta Sajdak <crappylime@gmail.com>
2017-05-17 00:16:55 +0200

    Implement Event class

---
+++ src/main/java/model/Event.java
@@ -0,0 +1,62 @@
+package model;
+
+import java.util.Date;
+
+/**
+ * Created by crappy on 12.05.17.
+ */
+public class Event {
+    private Integer id;
+    private String name;
+    private String description;
+    private Date date;
+    private String category;
+
+    public Event(Integer id, String name, String description, Date date, String category) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.date = date;
+        this.category = category;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
